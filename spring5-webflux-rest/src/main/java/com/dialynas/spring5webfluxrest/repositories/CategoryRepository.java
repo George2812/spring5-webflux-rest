d999dedf9fd78e391739a3db48551b7961d1c4da
George Dialynas <dialynas1@yahoo.com>
2020-10-16 16:25:11 +0300

    added Category Data and Repository

---
+++ spring5-webflux-rest/src/main/java/com/dialynas/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.dialynas.spring5webfluxrest.repositories;
+
+import com.dialynas.spring5webfluxrest.domain.Category;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category, String> {
+}
